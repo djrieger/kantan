6f69c8e400d7947471d4bbcc925f2318102b1f98
David Rieger <djrieger@users.noreply.github.com>
2014-03-05 13:06:56 +0100

    Updated resources plugin to 1.2.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -71,7 +71,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:3.6.10.8" // or ":hibernate4:4.3.1.1"
         runtime ":database-migration:1.3.8"
 		runtime ":jquery:1.11.0.1"
-        runtime ":resources:1.2.1"
+        runtime ':resources:1.2.2'
 		runtime ':twitter-bootstrap:3.0.3'
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
