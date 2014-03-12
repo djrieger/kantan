01a00f90d4926f0baa4f68d8f3be0a8fe3a57112
David Rieger <djrieger@users.noreply.github.com>
2014-03-12 14:35:57 +0100

    Updated BankAccount (removed constraint requiring each User to have a BankAccount)

--- grails-app/domain/de/kantan/BankAccount.groovy
+++ grails-app/domain/de/kantan/BankAccount.groovy
@@ -17,7 +17,7 @@ class BankAccount {
 //	Date	lastUpdated
 	
 	static	belongsTo	= [ user: User ]	// tells GORM to cascade commands: e.g., delete this object if the "parent" is deleted.
-	static	hasOne		= [ User ]	// tells GORM to associate another domain object as an owner in a 1-1 mapping
+//	static	hasOne		= [ User ]	// tells GORM to associate another domain object as an owner in a 1-1 mapping
 //	static	hasMany		= []	// tells GORM to associate other domain objects for a 1-n or n-m mapping
 //	static	mappedBy	= []	// specifies which property should be used in a mapping 
 	
@@ -27,7 +27,7 @@ class BankAccount {
 	static	constraints = {
 		accountHolder blank: false
 		iban size: 4..34
-		bic nullable: true
+		bic nullable: true, size: 8..11
     }
 	
 	/*
